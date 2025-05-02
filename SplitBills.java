3ca942c5530fa06cfea1ae369fe093350a003caa
tizzifona <tizzifona@gmail.com>
2025-05-02 12:03:03 +0200

    Added code for exercise "Split Bills"

--- SplitBills.java
+++ SplitBills.java
@@ -1,11 +1,8 @@
 public class SplitBills {
     public static void main(String[] args) {
-
-        //Crea dos variables, la primera que contenga el total de la cuenta que será 7 EUR y la segunda que contenga el número de personas a dividir (esta variable deberá ser de tipo int) que serán 2 personas.
-
-        //Haz la operación aritmética para devolver la cantidad a pagar por persona y almacénala dentro de una variable (del tipo que veas conveniente).
-        
-        //Imprime el resultado (deberá devolverte 3.5)
-
+        double totalBill = 7.0;
+        int numberOfPersons = 2;
+        double totalPerPerson = totalBill/numberOfPersons;
+        System.out.println("Total per person: " + totalPerPerson + " EUR.");
     }
 }
